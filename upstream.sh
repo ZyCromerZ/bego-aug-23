a4e6221ee907a30785d6c396ec3389a4d4ea54c7
ZyCromerZ <neetroid97@gmail.com>
2023-06-10 03:12:21 +0700

    upstream.sh: update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- upstream.sh
+++ upstream.sh
@@ -1,14 +1,19 @@
+git reset --hard
+stop="n"
 for asu in r-oss-base-release r-oss-base-release-uv
 do
-    git checkout $asu && git pull . r-oss-base --signoff --no-commit || break
+    [[ "$stop" == "y" ]] && break
+    git checkout $asu && git pull . r-oss-base --signoff --no-commit || stop="y"
     git commit -sm "Merge Branch 'r-oss-base' into $asu"
-    git pull . r-oss-up --signoff --no-commit || break
+    git pull . r-oss-up --signoff --no-commit || stop="y"
     git commit -sm "Merge Branch 'r-oss-up' into $asu"
-    git checkout $asu-ALMK && git pull . $asu --signoff --no-ff --no-commit || break
+    [[ "$stop" == "y" ]] && break
+    git checkout $asu-ALMK && git pull . $asu --signoff --no-ff --no-commit || stop="y"
     git commit -sm "Merge Branch '$asu' into $asu-ALMK"
-    git checkout $asu-SLMK && git pull . $asu --signoff --no-ff --no-commit || break
+    [[ "$stop" == "y" ]] && break
+    git checkout $asu-SLMK && git pull . $asu --signoff --no-ff --no-commit || stop="y"
     git commit -sm "Merge Branch '$asu' into $asu-SLMK"
-    git pull . r-oss-up-SLMK --signoff --no-commit || break
+    git pull . r-oss-up-SLMK --signoff --no-commit || stop="y"
     git commit -sm "Merge Branch 'r-oss-up-SLMK' into $asu-SLMK"
 done
 
@@ -17,5 +22,8 @@ DoPush()
     get="${@}"
     git push --all $get -f || DoPush $get
 }
-DoPush origin
-DoPush public
+
+if [[ "$stop" == "n" ]];then
+    DoPush origin
+    # DoPush public
+fi
