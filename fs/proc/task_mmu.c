37fc33a81bd85aae7054fe6d4bbab5b6ec03e142
ZyCromerZ <neetroid97@gmail.com>
2023-12-15 15:51:26 +0700

    Revert "fs: proc: task_mmu: add sultanpid option for tid"
    This reverts commit 68ac380fb7ef993e8f6142aa35e8bb9f59414163.
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -772,13 +772,6 @@ static const struct seq_operations proc_tid_maps_op = {
 	.show	= show_tid_map
 };
 
-static const struct seq_operations proc_tid_maps_op_sultanpid = {
-	.start	= m_start_pid,
-	.next	= m_start_pid,
-	.stop	= m_stop,
-	.show	= show_tid_map
-};
-
 static int pid_maps_open(struct inode *inode, struct file *file)
 {
 	if (sultan_pid)
@@ -789,10 +782,7 @@ static int pid_maps_open(struct inode *inode, struct file *file)
 
 static int tid_maps_open(struct inode *inode, struct file *file)
 {
-	if (sultan_tid)
-		return do_maps_open(inode, file, &proc_tid_maps_op_sultanpid);
-	else
-		return do_maps_open(inode, file, &proc_tid_maps_op);
+	return do_maps_open(inode, file, &proc_tid_maps_op);
 }
 
 const struct file_operations proc_pid_maps_operations = {
@@ -1288,13 +1278,6 @@ static const struct seq_operations proc_tid_smaps_op = {
 	.show	= show_tid_smap
 };
 
-static const struct seq_operations proc_tid_smaps_op_sultanpid = {
-	.start	= m_start_pid,
-	.next	= m_next_pid,
-	.stop	= m_stop,
-	.show	= show_tid_smap
-};
-
 static int pid_smaps_open(struct inode *inode, struct file *file)
 {
 	if (sultan_pid_smap)
@@ -1324,10 +1307,7 @@ static int pid_smaps_rollup_open(struct inode *inode, struct file *file)
 
 static int tid_smaps_open(struct inode *inode, struct file *file)
 {
-	if (sultan_tid_smap)
-		return do_maps_open(inode, file, &proc_tid_smaps_op_sultanpid);
-	else
-		return do_maps_open(inode, file, &proc_tid_smaps_op);
+	return do_maps_open(inode, file, &proc_tid_smaps_op);
 }
 
 const struct file_operations proc_pid_smaps_operations = {
@@ -2473,13 +2453,6 @@ static const struct seq_operations proc_tid_numa_maps_op = {
 	.show   = show_tid_numa_map,
 };
 
-static const struct seq_operations proc_tid_numa_maps_op_sultanpid = {
-	.start  = m_start_pid,
-	.next   = m_next_pid,
-	.stop   = m_stop,
-	.show   = show_tid_numa_map,
-};
-
 static int numa_maps_open(struct inode *inode, struct file *file,
 			  const struct seq_operations *ops)
 {
@@ -2497,10 +2470,7 @@ static int pid_numa_maps_open(struct inode *inode, struct file *file)
 
 static int tid_numa_maps_open(struct inode *inode, struct file *file)
 {
-	if (sultan_tid_map)
-		return numa_maps_open(inode, file, &proc_tid_numa_maps_op_sultanpid);
-	else
-		return numa_maps_open(inode, file, &proc_tid_numa_maps_op);
+	return numa_maps_open(inode, file, &proc_tid_numa_maps_op);
 }
 
 const struct file_operations proc_pid_numa_maps_operations = {
